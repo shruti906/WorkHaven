32c349c0ecccd5444a8b9b8e164bccf121e86aab
shruti906 <shruti.keshri.23cse@bmu.edu.in>
2024-11-29 19:40:05 +0530

    used routes

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 const PORT = 3000;
+const routes = require('./routes/routes')
 
+app.use('/',routes)
+app.use(express.static('./assets'));
+app.use(express.static('./scripts'));
 // Middleware to parse JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,33 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
 // Default route to serve home.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'home.html'));
-});
 
-
-// POST route to handle login form submission
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    const loginData = { email, password };
-
-    const filePath = path.join(__dirname, 'login.json');
-
-    // Read existing data
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        let json = [];
-        if (!err && data) {
-            json = JSON.parse(data);
-        }
-        json.push(loginData);
-        fs.writeFile(filePath, JSON.stringify(json, null, 2), (err) => {
-            if (err) {
-                return res.status(500).send('Error saving data');
-            }
-            res.redirect('home.html');
-        });
-    });
-});
 
 // Start the server
 app.listen(PORT, () => {
---
+++ routes/routes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router= express.Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+
+
+
+
+
+
+
+// POST route to handle login form submission
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    const loginData = { email, password };
+
+    const filePath = path.join(__dirname, 'login.json');
+
+    // Read existing data
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        let json = [];
+        if (!err && data) {
+            json = JSON.parse(data);
+        }
+        json.push(loginData);
+        fs.writeFile(filePath, JSON.stringify(json, null, 2), (err) => {
+            if (err) {
+                return res.status(500).send('Error saving data');
+            }
+            res.redirect('home.html');
+        });
+    });
+});
+
+module.exports = router;
