d8b5c73edb66b7899decc0b0f9ba9c230cf2ffbd
shruti906 <shruti.keshri.23cse@bmu.edu.in>
2024-12-02 11:52:25 +0530

    Revert "deleted node modules"
    This reverts commit d24f928262c9c190dea9af7c9f257998fcb59926.

---
+++ mongodb/connectdb.js
@@ -0,0 +1,2 @@
+let mongoose= require("mongoose")
+let con_string="mongodb+srv://shrutikeshri23cse:zBW5EKBqUYtf2gCq@studentcluster00.v7s0i.mongodb.net/?retryWrites=true&w=majority&appName=studentCluster00"
